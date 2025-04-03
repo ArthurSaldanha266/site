3d634136e0de756cf53bd78f68a0d2641d7f2be4
Arthur Saldanha <arthursilvaleao020606@gmail.com>
2025-04-02 23:06:49 -0300

    Add files via upload

---
+++ site.php
@@ -0,0 +1,223 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sld Soluções</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="site.css">
+
+
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+
+</head>
+<body>
+
+<div class="conteiner">
+	<div class="row">
+		<div class="col-sm-3 logo"> 
+	<P class="text-center">	#sld Soluções</P></div>
+
+<div class="col-sm-9 menu">
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark meumenu">
+  <a class="navbar-brand" href="#">#sld</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarnava1tmarkup" aria-controls="#navbarnava1tmarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="#navbarnava1tmarkup">
+    <div class="navbar-nav">
+
+     <a class="nav-link nav-link active " href="#">Home <span class="sr-only">(current)</span></a>
+     <a class="nav-item nav-link" href="#">Features</a>
+     <a class="nav-link nav-link" href="#">Pricing</a>
+     <a class="nav-item nav-link" href="#">disabled</a>
+
+  </div>
+</div>
+</nav>
+</div>
+</div>
+
+<div class="row">
+	<div class="col-sm-12 banner">
+		<img src="banner1.jpg" class="img-fluid" alt="imagem reponsiva">
+		<br>
+</div>
+</div>
+
+<script src="js/bootstrap.js"></script>
+
+<div class="row">
+	<div class="col-sm-3 c1">
+		<br>
+
+		<h2>desenvolvimento do saldanha pgs</h2>
+
+		<img src="img1.jpg" class="img-fluid" alt="imagem responsiva">
+		<br>
+		<p> a melhor solução de tecnologia </p>
+	</div>
+	<div class="col-sm-3 c2">
+		<br>
+		<h2>desenvolvimento de saldanha pgs</h2>
+
+		<img src="img1.jpg" class="img-fluid" alt="imagem responsiva">
+		<br>
+		<p>a melhor solução de tecnologia</p>
+</div>
+
+<div class="col-sm-3 c3">
+	<br>
+<h2>desenvolvimento de saldanha pgs</h2>
+
+		<img src="img1.jpg" class="img-fluid" alt="imagem responsiva">
+		<br>
+		<p>a melhor solução de tecnologia </p>
+</div>
+
+<div class="col-sm-3 c4">
+	<br>
+<h2>desenvolvimento de saldanha pgs</h2>
+
+		<img src="img1.jpg" class="img-fluid" alt="imagem responsiva">
+		<br>
+		<p>a melhor solução de tecnologia</p>
+</div>
+</div>
+
+<div class="row" style="background-color: transparent;">
+	
+<div class="col-sm-4">Audios</div>
+<div class="col-sm-8">formulario</div>
+
+
+</div></div></body>
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+<footer class="rodape" style="background-color: darkgrey;">
+  <!-- Section: Social media -->
+  <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+    <!-- Left -->
+    <div class="me-5 d-none d-lg-block">
+      <span>tudo sobre o nosso site da melhor tecnologia existente</span>
+    </div>
+
+    <div>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-facebook-f"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-twitter"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-google"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-instagram"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-linkedin"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-github"></i>
+      </a>
+    </div>
+    <!-- Right -->
+  </section>
+  <!-- Section: Social media -->
+
+  <!-- Section: Links  -->
+  <section class="rodape" style="background-color: darkgrey;">
+
+    <div class="container text-center text-md-start mt-5">
+      <!-- Grid row -->
+      <div class="row mt-3">
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <!-- Content -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            <i class="fas fa-gem me-3"></i>sobre
+          </h6>
+          <p>
+            neste site foi ultilizado html em css com bootstrap/js da uma maneira simplificada e bastante usado no mais comuns
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Tecnologia
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">html</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">css</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">php</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">bootstrap/js</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            complementos
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">principal</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">configurações</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">ordem</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">ajuda</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">Contato</h6>
+          <p><i class="fas fa-home me-3"></i> Brasilia/DF </p>
+          <p>
+            <i class="fas fa-envelope me-3"></i>
+            arthursilvaleao0206060@gmail.com
+          </p>
+          <p><i class="fas fa-phone me-3"></i> 061 993347133 </p>
+          <p><i class="fas fa-print me-3"></i> 061 993347133 </p>
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+    </div>
+  </section>
+  <!-- Section: Links  -->
+
+  <!-- Copyright -->
+  <div class="text-center p-4" style="background-color: black;">
+    
+   Copyright: © 2024 
+    <a class="text-reset fw-bold" href="https://mdbootstrap.com/">SLDBootstrap.com</a>
+  </div>
+  
+</footer>
+
+</body>
+</html>
